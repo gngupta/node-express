894b76d77e47b1d0b9c5f0ef888205dc4c6adbaa
Gorakh Nath Gupta <gng@nl-gorakh.local>
2019-06-24 12:49:51 +0200

    implemented socket, mongodb

--- server.js
+++ server.js
@@ -1,32 +1,59 @@
 var express = require("express")
 var bobyParser = require("body-parser")
-
 var app = express()
+var http = require("http").Server(app)
+var io = require("socket.io")(http)
+
+var mongoose = require("mongoose")
+var dbURL = "mongodb+srv://user:Passw0rd1@meanstack-2fupe.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(dbURL, {
+    useNewUrlParser: true
+}, (err) => {
+    if (err) {
+        console.log("Mongo db connection error :: " + err)
+    } else {
+        console.log("Mongo db connection successful")
+    }
+})
+
 app.use(express.static(__dirname))
 app.use(bobyParser.json())
-app.use(bobyParser.urlencoded({extended:false}))
+app.use(bobyParser.urlencoded({
+    extended: false
+}))
 
-var messages = [
-    {
-        name : "Shiva",
-        message : "Immortals of meluha"
-    },
-    {
-        name : "Vishnu",
-        message : "Immortals of Devagiri"
-    }
-]
-
-app.get("/messages",(req, res) => {
-    res.send(messages)
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
 
-app.post("/message", (req, res)=>{
-    console.log(req.body)
-    messages.push(req.body)
-    res.sendStatus(200)
+app.get("/messages", (req, res) => {
+    Message.find({}, (err, messages) => {
+        if (err) {
+            console.log("Error occured while finding messages from mongo db")
+        } else {
+            res.send(messages)
+        }
+    })
 })
 
-var server = app.listen(3000, () => {
+app.post("/message", (req, res) => {
+    var message = new Message(req.body)
+    message.save((err) => {
+        if (err) {
+            console.log("Failed to save in mongo db")
+            res.sendStatus(500)
+        } else {
+            io.emit('message', req.body)
+            res.sendStatus(200)
+        }
+    })
+})
+
+io.on("connection", (socket) => {
+    console.log("A user connected")
+})
+
+var server = http.listen(3000, () => {
     console.log("Server is listening on port :: " + server.address().port)
 });
